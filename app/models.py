6ad068d6c99a1060511e2995f84a112024dec28f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-18 19:43:47 +0300

    creating class models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class Pitch:
     all_pitches = []
 
@@ -12,3 +14,23 @@ class Pitch:
     @classmethod
     def clear_pitches(cls):
         Pitches.all_pitches.clear()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    pass_hash = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.title}'
