9006b58fdd34be410c5a13b764d39c018eb97d18
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-18 20:38:10 +0300

    adding forms.py file for authentication

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Pitch:
     all_pitches = []
@@ -16,10 +22,11 @@ class Pitch:
     def clear_pitches(cls):
         Pitches.all_pitches.clear()
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     pass_hash = db.Column(db.String(255))
 
