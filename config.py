4b56ce82d93d9a05496dbb9dcd0dd4610a63400b
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-21 14:49:09 +0300

    updates

--- config.py
+++ config.py
@@ -1,43 +1,27 @@
 import os
+
 class Config:
-    '''
-    General configuration parent class
-    '''
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Computer7575@localhost/pitches'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("tonuikipkorir1@gmail.com")
+    MAIL_PASSWORD = os.environ.get("password")
 
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-    DEBUG = True
+    pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
 config_options = {
