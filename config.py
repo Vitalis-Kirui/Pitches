1ab09bf784784dbafa960c6abb4fb237bbb2c2a8
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-21 16:02:55 +0300

    app to vitz

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("tonuikipkorir1@gmail.com")
-    MAIL_PASSWORD = os.environ.get("password")
+    MAIL_USERNAME = os.environ.get("vitaliskirui1@gmail.com")
+    MAIL_PASSWORD = os.environ.get("Computer757575.")
 
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='password'
-export MAIL_USERNAME='maybethisiswhereibelong@gmail.com'
+export MAIL_USERNAME='vitaliskirui1@gmail.com'
 export MAIL_PASSWORD='Computer757575.'
 
 python3.8 manage.py server
